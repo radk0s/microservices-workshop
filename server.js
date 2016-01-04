f2247131dd6eb69097c0b90f41d358264d9b8083
rchamot <radoslaw.chamot@schibsted.pl>
2016-01-04 11:43:21 +0100

    step 4 - error handling

--- server.js
+++ server.js
@@ -2,19 +2,30 @@ const express = require('express');
 const app = express();
 
 const logRequest = (req, res, next) => {
-    console.log('log req');
     next();
 };
 
-const logRequest2 = (req, res, next) => {
-    console.log('log req 2');
-    next();
-};
-
-app.use(logRequest)
 app.get('/', logRequest, function (req, res) {
     res.send('Hello World!');
 });
+
+app.use(clientError);
+app.use(serverError);
+
+function clientError(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+}
+
+function serverError(err, req, res, next) {
+    res.status(err.status || 500);
+    console.error(err.stack);
+    res.json({
+        message: err.message,
+        error: (process.env.NODE_ENV === 'production') ? {} : err.toString()
+    });
+}
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
