42c4b68b594fc3c2997ab67b890de7f9c7f34126
rchamot <radoslaw.chamot@schibsted.pl>
2016-01-04 10:56:50 +0100

    step 1 - update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const http = require('http');
 const server = http.createServer((req, res) => res.end('Hello'));
 
-server.listen(3000);
+server.listen(3000, () => console.log("Server started on port: " + 3000));
